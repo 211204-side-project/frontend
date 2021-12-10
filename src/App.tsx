7bcb235e8dbb5baf3df8efeaf075f56d501ff0d9
DONG WOONG OH <integral5137@gmail.com>
2021-12-11 00:55:29 +0900

    set up to react table settttttt

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routers from './routes/Routers'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+      <main>
+        <Routers />
+      </main>
+    </section>
+  )
 }
 
-export default App;
+export default App
