c9b78fe0e63b705c138be11c5e4737ae20d6196b
DONG WOONG OH <integral5137@gmail.com>
2021-12-11 12:10:52 +0900

    create to file the footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Routers from './routes/Routers'
 import Navigators from './pages/Navigators'
+import Routers from './routes/Routers'
 
 const App = () => {
   return (
@@ -10,6 +10,7 @@ const App = () => {
       <main>
         <Routers />
       </main>
+      <footer></footer>
     </section>
   )
 }
