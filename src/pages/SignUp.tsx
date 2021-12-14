5b2d178ebee8a6faa1ab6c8a80cd012796387d63
DONG WOONG OH <integral5137@gmail.com>
2021-12-14 19:24:52 +0900

    add to create the sign up form by react hook form

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,119 @@
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import FormError from '../components/errors/FormError'
+
+const SignUp = () => {
+  const navigator = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = async () => {}
+
+  return (
+    <section>
+      <Helmet>
+        <title>Create account</title>
+      </Helmet>
+
+      <h1>User Sign up!</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input
+            {...register('accountId', {
+              required: 'You must specify a account id',
+              pattern: {
+                value: /^[A-za-z0-9]{4,15}$/,
+                message: 'Please insert a valid account id',
+              },
+            })}
+            type="text"
+            name="accountId"
+            placeholder="Account ID"
+            autoComplete="off"
+          />
+          <button>Check</button>
+          {errors.accountId && (
+            <FormError errorMessage={errors.accountId.message!} />
+          )}
+        </div>
+        <div>
+          <input
+            {...register('password', {
+              required: 'You must specify a password',
+              pattern: {
+                value: /(?=.*\d)(?=.*[a-z]).{8,}/,
+                message: 'Password must have at least 8 characters',
+              },
+            })}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          {errors.password && (
+            <FormError errorMessage={errors.password.message!} />
+          )}
+        </div>
+        <div>
+          <input
+            {...register('confirmPassword', {
+              validate: (v) =>
+                v === watch('password') || 'The passwords do not match',
+            })}
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+          />
+          {errors.confirmPassword?.message && (
+            <FormError errorMessage={errors.confirmPassword.message} />
+          )}
+        </div>
+        <div>
+          <input
+            {...register('email', {
+              required: 'You must specify a email',
+              pattern: {
+                value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/,
+                message: 'Please insert a valid email address',
+              },
+            })}
+            type="email"
+            name="email"
+            placeholder="Email"
+            autoComplete="off"
+          />
+          <button>Check</button>
+          {errors.email && <FormError errorMessage={errors.email.message!} />}
+        </div>
+        <div>
+          <input
+            {...register('nickname', {
+              required: 'You must specify a nickname',
+              pattern: {
+                value: /^[A-za-z0-9]{2,12}$/,
+                message: 'Please insert a valid nickname',
+              },
+            })}
+            type="text"
+            name="nickname"
+            placeholder="Nickname"
+            autoComplete="off"
+          />
+          <button>Check</button>
+          {errors.nickname && (
+            <FormError errorMessage={errors.nickname.message!} />
+          )}
+        </div>
+
+        <input type="submit" value="Sign up" />
+      </form>
+    </section>
+  )
+}
+
+export default SignUp
