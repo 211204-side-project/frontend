9ee1df3e5ce117b1010de0f39e2e42bc6cd0bdf0
DONG WOONG OH <integral5137@gmail.com>
2021-12-14 22:35:37 +0900

    Test code render app form error

---
+++ src/__test__/app.spec.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import { HelmetProvider } from 'react-helmet-async'
+import { BrowserRouter } from 'react-router-dom'
+import App from '../App'
+
+describe('app render', () => {
+  it('should ', () => {
+    const { debug } = render(
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>,
+    )
+    debug()
+  })
+})
---
+++ src/__test__/form-error.spec.ts
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import FormError from '../components/errors/FormError'
+
+describe('Form error', () => {
+  it('test error message props', () => {
+    // const { getByText } = render(<FormError errorMessage={'test'} />)
+    // getByText('test')
+  })
+})
