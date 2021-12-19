181f57ffb5b464637b3f52ca6535256810878d58
LimJungMyeong <freewjdaud@gmail.com>
2021-12-19 19:32:49 +0900

    main page update

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Wrapper_style1,
   Inner,
@@ -8,9 +8,33 @@ import {
   ImageFit,
   Caption,
   Wrapper_style2,
+  Flex_tabs,
+  Tabs,
+  Tab,
+  Col,
+  Wrapper_style3,
 } from '../pages/styles/MainComponent.style'
 
 const MainComponent = () => {
+  const [actFirst, setActFirst] = useState<boolean>(true)
+  const [actSecond, setActSecond] = useState<boolean>(false)
+  const [actThird, setActThird] = useState<boolean>(false)
+
+  const TabFirst = () => {
+    setActFirst(true)
+    setActSecond(false)
+    setActThird(false)
+  }
+  const TabSecond = () => {
+    setActFirst(false)
+    setActSecond(true)
+    setActThird(false)
+  }
+  const TabThird = () => {
+    setActFirst(false)
+    setActSecond(false)
+    setActThird(true)
+  }
   return (
     <>
       <Wrapper_style1 className="section-2">
@@ -46,10 +70,178 @@ const MainComponent = () => {
       <Wrapper_style2 className="section-3">
         <Inner>
           <Header>
-            <img src="assets/images/pic07.jpg"></img>
+            <h2>Etiam veroeros lorem</h2>
+            <p>
+              Fusce fringilla enim sit amet lectus sollicitudin, eu tincidunt
+              odio semper.
+            </p>
           </Header>
+          <Flex_tabs>
+            <ul>
+              <li onClick={TabFirst}>
+                <a className={actFirst ? 'active' : undefined}>
+                  Duis vestibulum tellus
+                </a>
+              </li>
+              <li onClick={TabSecond}>
+                <a className={actSecond ? 'active' : undefined}>
+                  Quam tempus sodales ipsum
+                </a>
+              </li>
+              <li onClick={TabThird}>
+                <a className={actThird ? 'active' : undefined}>
+                  Donec faucibus risus cursus
+                </a>
+              </li>
+            </ul>
+            <Tabs>
+              {/* Tab1  */}
+              {actFirst ? (
+                <Tab>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic07.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic07.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic07.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic07.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic07.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic07.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                </Tab>
+              ) : undefined}
+              {/* Tab2 */}
+              {actSecond ? (
+                <Tab>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic08.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic08.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic08.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic08.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic08.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic08.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                </Tab>
+              ) : undefined}
+              {/* Tab3 */}
+              {actThird ? (
+                <Tab>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic10.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic10.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic10.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic10.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic10.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                  <Col>
+                    <ImageFit className="tab-img">
+                      <img src="assets/images/pic10.jpg"></img>
+                    </ImageFit>
+                  </Col>
+                </Tab>
+              ) : undefined}
+            </Tabs>
+          </Flex_tabs>
         </Inner>
       </Wrapper_style2>
+      <Wrapper_style3 className="section-4">
+        <Inner>
+          <Header>
+            <h2>Aliquam ipsum purus dolor</h2>
+            <p>
+              Cras sagittis turpis sit amet est tempus, sit amet consectetur
+              purus tincidunt.
+            </p>
+          </Header>
+          <FlexBoxs>
+            <Col>
+              <ImageFit className="section3-img">
+                <img src="assets/images/pic09.jpg"></img>
+              </ImageFit>
+              <Caption>
+                Pellentesque at nunc vitae urna suscipit mollis nec in arcu
+              </Caption>
+            </Col>
+            <Col>
+              <ImageFit className="section3-img">
+                <img src="assets/images/pic10.jpg"></img>
+              </ImageFit>
+              <Caption>
+                Morbi mattis ligula ut eros ipsum aliquam iaculis dictum
+                suscipit
+              </Caption>
+            </Col>
+            <Col>
+              <ImageFit className="section3-img">
+                <img src="assets/images/pic11.jpg"></img>
+              </ImageFit>
+              <Caption>
+                Pellentesque at nunc vitae urna suscipit mollis nec in arcu
+              </Caption>
+            </Col>
+          </FlexBoxs>
+        </Inner>
+      </Wrapper_style3>
     </>
   )
 }
--- src/pages/styles/MainComponent.style.ts
+++ src/pages/styles/MainComponent.style.ts
@@ -131,4 +131,132 @@ export const Wrapper_style2 = styled.section`
     opacity: 0.35;
     z-index: 1;
   }
+  Header {
+    display: block;
+    text-align: left;
+    margin-bottom: 4em;
+    h2 {
+      font-size: 2.5em;
+      font-weight: 700;
+      line-height: 1.5;
+      margin: 0 0 1em 0;
+    }
+    p {
+      font-size: 1.25em;
+    }
+  }
+`
+
+export const Flex_tabs = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+
+  ul {
+    width: 20%;
+    list-style: none;
+    margin: 1.5em 0 1em 0;
+    padding: 0;
+    @media screen and (max-width: 980px) {
+      width: 30%;
+    }
+  }
+  li {
+    padding-left: 0.5em;
+    display: list-item;
+    list-style: none;
+  }
+  a {
+    background: rgba(255, 255, 255, 0.075);
+    color: #c1c7cd;
+    border-radius: 4px;
+    margin-bottom: 0.5em;
+    padding: 1em 1em;
+    display: inline-block;
+    width: 100%;
+    transition: color 0.2s ease-in-out;
+    text-decoration: none;
+  }
+  a:hover {
+    cursor: pointer;
+    color: #ffffff;
+    background: #693dcc;
+  }
+  .active {
+    background: #aa14cc;
+    color: #fff !important;
+  }
+`
+
+export const Tabs = styled.div`
+  width: 77%;
+  @media screen and (max-width: 980px) {
+    width: 67%;
+  }
+`
+
+export const Tab = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: stretch;
+`
+export const Col = styled.div`
+  width: 31%;
+  background: rgba(255, 255, 255, 0.075);
+  border-radius: 4px;
+  margin: 0 0 2em 0;
+  overflow: hidden;
+  position: relative;
+  .tab-img {
+    max-height: 196px;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    transition: transform 0.2s ease-in-out;
+  }
+  &:hover {
+    cursor: pointer;
+    img {
+      height: 100%;
+      transform: scale(1.075);
+    }
+  }
+`
+
+export const Wrapper_style3 = styled.section`
+  background-color: #061014;
+  color: #c1c7cd;
+  width: 100%;
+  height: 100vh;
+  padding: 6em 0 4em 0;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  scroll-snap-align: start;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    display: block;
+    background-image: url(../../../assets/images/bg.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    opacity: 0.35;
+    z-index: 1;
+  }
+
+  .section3-img {
+    margin: 0;
+    max-height: 100%;
+    position: relative;
+    overflow: hidden;
+    display: block;
+    width: 100%;
+  }
 `
