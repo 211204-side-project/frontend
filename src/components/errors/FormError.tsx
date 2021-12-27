a14729ebaeea726e612f29686117e145a626d11a
moguri846 <dkffkekahsl1@naver.com>
2021-12-27 17:51:08 +0900

    chore !: FormError modify css
    FormError modify css

--- src/components/errors/FormError.tsx
+++ src/components/errors/FormError.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 interface IFormErrorProps {
   errorMessage: string
 }
 
+const Warning = styled.span`
+  height: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 13px;
+  color: red;
+`
+
 const FormError: React.FC<IFormErrorProps> = ({ errorMessage }) => (
-  <div role="alert">{errorMessage}</div>
+  <Warning role="alert">{errorMessage}</Warning>
 )
 
 export default FormError
